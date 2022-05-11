190aeb1795cf5af14465a16897cd697b5d890adb
AlejandroAriza13 <alejandro-2030@hotmail.com>
2022-05-11 17:26:35 -0500

    Add examples files VHDL

---
+++ Day_one.py
@@ -0,0 +1,58 @@
+# #Welcome go to run 30 days of python challenge
+# #Challenge one
+# print(3 + 4) #Suma
+# print(3 - 4) #Rest
+# print(3 * 4) #Multiplication
+# print(3 ** 4) #Exponential
+# print(3 / 4) #Division
+# print(3 // 4) #Division integer
+# print(3 % 4) # module
+
+# #Challenge two
+# print ("Alejandro")
+# print ("Ariza Mojica")
+# print ("Colombia")
+# print ("Estoy disfrutando de 30 días de python")
+
+# #Challenge three
+# print(type(10))
+# print(type(9.8))
+# print(type(3.14))
+# print(type(4 - 4j))
+# print(type(["Alejandro", "Python", "Colombia"]))
+# print(type("Alejandro"))
+# print(type("Ariza"))
+# print(type("Colombia"))
+
+# #Exercise three
+# #Example interger
+# print(type(25))
+
+# #Example float
+# print(type(10.1))
+
+# #Example complex
+# print(type(45 + 25j))
+
+# #Example str
+# print(type("I'm crazy"))
+
+# #Example boolean 
+# print(type(True))
+
+# #Example list
+# print(type([1, 2, 3, 4, 5]))
+
+# #Example tuple
+# print(type(("Alejandro", "Andres", "Daniel", "Felipe")))
+
+# #Example set
+# print (type({2, 1, 2.5, 14}))
+
+# #Example dictionary
+# print(type({"Seré" : "Un gran desarrollador de software"})) 
+
+#Exercise level three
+
+euclidiana = float((2-10)**2+(3-8)**2)
+print("La distancia euclidiana entre los dos puntos es: ", euclidiana**0.5)
